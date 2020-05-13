62349869b54dbdd7e7327e2017fac1f9d68d5051
Vanesa <germadevanesa@gmail.com>
2020-05-13 00:59:44 -0300

    first commit

---
+++ src/com/tokenitos/superlist/Main.java
@@ -0,0 +1,9 @@
+package com.tokenitos.superlist;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Bienvenido a superlist!");
+    }
+}
