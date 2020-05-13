c7bb3dd43e281700ff9bfd8e0f72b582362b02e6
Vanesa <germadevanesa@gmail.com>
2020-05-13 02:50:16 -0300

    lista ciclo y articulo

---
+++ super/src/com/tokenitos/superlist/Articulo.java
@@ -0,0 +1,17 @@
+public class Articulo{
+private final String articulo;
+private final int cantidad;
+
+public Articulo(String articulo, int cantidad)
+{
+	this.articulo=articulo;
+	this.cantidad=cantidad;
+}
+
+public String getArticulo(){return articulo;}
+public int getCantidad(){return cantidad;}
+
+
+
+}
+
--- super/src/com/tokenitos/superlist/Main.java
+++ super/src/com/tokenitos/superlist/Main.java
@@ -1,9 +1,23 @@
 package com.tokenitos.superlist;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Bienvenido a superlist!");
+	menu();
+    }
+
+    public static void menu(){
+
+        Scanner in = new Scanner(System.in);
+        Scanner inN = new Scanner(System.in);
+        System.out.println("¿Qué deseas comprar?");
+        String s = in.nextLine();
+	System.out.println("¿Qué deseas comprar?");
+	String n = inN.nextLine();
+
+
+	
     }
 }
