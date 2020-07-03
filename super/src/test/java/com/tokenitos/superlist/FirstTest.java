c591a90a640c3d9fab274c86a05fa32c24bb3146
vanesa <germadevanesa@gmail.com>
2020-07-03 10:59:55 -0300

    dos test triviales para agregar la dependencia en mvn

---
+++ super/src/test/java/com/tokenitos/superlist/FirstTest.java
@@ -0,0 +1,39 @@
+package com.tokenitos.superlist;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.tokenitos.superlist.Main.isNumeric;
+import static com.tokenitos.superlist.Main.validarVacio;
+
+
+public class FirstTest {
+
+
+    @Test
+    @DisplayName("Test successful parse string to int conversion")
+    void inNumericTest() {
+        String cadena = "123";
+        Assertions.assertTrue(isNumeric(cadena));
+
+    }
+
+    @Test
+    @DisplayName("Test successful validar vacio")
+    void validarVacioTest() {
+        String cadena = "";
+        Assertions.assertTrue(validarVacio(cadena));
+
+    }
+   /* @Test
+    void testConvertToDecimalSuccess() {
+        double result = MathTools.convertToDecimal(3, 4);
+        Assertions.assertEquals(0.75, result);
+    }
+
+    @Test
+    void testConvertToDecimalInvalidDenominator() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> MathTools.convertToDecimal(3, 0));
+    }*/
+}
